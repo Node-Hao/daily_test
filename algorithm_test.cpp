278401f42a8c3e65c4021ca82421cece5d0f5aa8
Node-Hao <yunc58451@gmail.com>
2025-11-06 21:14:28 +0800

    11_16

--- algorithm_test.cpp
+++ algorithm_test.cpp
@@ -2093,3 +2093,17 @@ public:
         return max;
     }
 };
+// 合并二叉树
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) 
+    {
+        if (!root1) return root2;
+        if (!root2) return root1;
+        TreeNode* root = new TreeNode(root1->val + root2->val);
+
+        root->left = mergeTrees(root1->left, root2->left);
+        root->right = mergeTrees(root1->right, root2->right);
+        return root;
+    }
+};
